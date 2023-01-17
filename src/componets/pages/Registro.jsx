897daaa701e2616ffc601f3f5c5386f008d0c9d7
Luis Llancamil <luis.llancamil.a@gmail.com>
2023-01-17 05:01:32 -0300

    Actualizacion

--- src/componets/pages/Registro.jsx
+++ src/componets/pages/Registro.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Button, Card, Container, FloatingLabel, Form, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import s from '../../css/registro.module.css';
+import icLogo from '../images/ic_logo_tester.svg';
 
 const Registro = () => {
     return (
-        <Container>
-            <Card border="dark border border-2">
-                <Card.Header>
-                    Cabecera
-                </Card.Header>
+        <Container className='my-2'>
+            <Card className={`${s.registrocard} m-auto`}>
                 <Card.Body>
-                    cuerpo
+                    <div className='text-center'>
+                        <Image className='btn-primary' rounded={false} src={icLogo} width='100px' />
+                    </div>
+
+                    <Card.Title className='text-center my-3'>Registrarme</Card.Title>
+                    <Form>
+                        <FloatingLabel className='mb-3' controlId='floatingInput' label='Nombre'>
+                            <Form.Control type='text' placeholder='Juan' />
+                        </FloatingLabel>
+                        <FloatingLabel className='mb-3' controlId='floatingInput' label='Apellido'>
+                            <Form.Control type='text' placeholder='Perez' />
+                        </FloatingLabel>
+                        <FloatingLabel className='mb-3' controlId='floatingInput' label='Correo Electronico'>
+                            <Form.Control type='email' placeholder='micorreo@example.com' />
+                        </FloatingLabel>
+                        <FloatingLabel className='mb-3' controlId='floatingPassword' label='Contraseña'>
+                            <Form.Control type='password' placeholder='Contraseña' />
+                        </FloatingLabel>
+                        <Button className='float-end' variant='primary' type='submit'>
+                            Registrarme
+                        </Button>
+                    </Form>
+                    <Link className='navbar-brand text-success' to='#'>
+                        Ya tengo una Cuenta
+                    </Link>
                 </Card.Body>
-                <Card.Footer>
-                    pie
-                </Card.Footer>
             </Card>
         </Container>
     );
--- src/redux/reducer/authReducer.js
+++ src/redux/reducer/authReducer.js
@@ -1,12 +1,12 @@
 import { AUTH_NEED_VERIFICATION, AUTH_SET_ERROR, AUTH_SET_LOADING, AUTH_SET_SUCCESS, AUTH_SET_USER, AUTH_SIGN_OUT } from '../types/authTypes';
 
 const initialState = {
-    usuario: null,
-    authenticated: false,
-    loading: false,
-    error: '',
-    needVerification: false,
-    success: '',
+    usuarioAuth: null,
+    authenticatedAuth: false,
+    loadingAuth: false,
+    errorAuth: '',
+    needVerificationAuth: false,
+    successAuth: '',
   };
 
   const authReducer = (state = initialState, action) => {
@@ -14,35 +14,35 @@ const initialState = {
       case AUTH_SET_USER:
         return {
           ...state,
-          user: action.payload,
-          authenticated: true,
+          usuarioAuth: action.payload,
+          authenticatedAuth: true,
         };
       case AUTH_SET_LOADING:
         return {
           ...state,
-          loading: action.payload,
+          loadingAuth: action.payload,
         };
       case AUTH_SIGN_OUT:
         return {
           ...state,
-          user: null,
-          authenticated: false,
-          loading: false,
+          usuarioAuth: null,
+          authenticatedAuth: false,
+          loadingAuth: false,
         };
       case AUTH_SET_ERROR:
         return {
           ...state,
-          error: action.payload,
+          errorAuth: action.payload,
         };
       case AUTH_NEED_VERIFICATION:
         return {
           ...state,
-          needVerification: true,
+          needVerificationAuth: true,
         };
       case AUTH_SET_SUCCESS:
         return {
           ...state,
-          success: action.payload,
+          successAuth: action.payload,
         };
       default:
         return state;
