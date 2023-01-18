35f8011c1c2b89994254800ca9ef22f306ba777c
Mariano Araoz Teran <113387906+dogoarg@users.noreply.github.com>
2023-01-18 20:39:35 -0300

    generos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './componets/pages/Layout';
 import Home from './componets/pages/Home';
 import Registro from './componets/pages/Registro';
+import Generos from './componets/pages/Generos';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
         <Route path='registro' element={<Registro />} />
+        <Route path='generos' element={<Generos />} />
       </Route>
     </Routes>
   );
