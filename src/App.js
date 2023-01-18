234ee2fcb382edf25724f0f96ee3b6b541bd4d04
Serblaor <sergio.bo7@hotmail.com>
2023-01-18 17:43:51 -0500

    perfiles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './componets/pages/Layout';
 import Home from './componets/pages/Home';
 import Registro from './componets/pages/Registro';
+import PerfilUS from './componets/pages/PerfilU';
+import PerfilAd from './componets/pages/PerfilAd'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
         <Route path='registro' element={<Registro />} />
+        <Route path='PerfilUsuario' element={<PerfilUS />} />
+        <Route path='PerfilAdmin' element={<PerfilAd />} />
       </Route>
     </Routes>
   );
