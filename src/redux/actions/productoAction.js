b52e2d584431c4a24b8874f83fdfbb51c8e93f1e
RonaldoMonki <ronalldocorrea@gmail.com>
2023-01-18 23:22:54 -0500

    commit

--- src/redux/actions/productoAction.js
+++ src/redux/actions/productoAction.js
@@ -1,8 +1,7 @@
-import { allAuth, allDb, auth, db } from '../../firebaseInicial/firebase';
+import { allAuth, allDb, auth, db } from "../../firebaseInicial/firebase";
 
 const lista_Productos = await allDb.getDocs(allDb.collection(db, "producto"));
 
 lista_Productos.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
+  console.log(doc.id, " => ", doc.data());
 });
-
