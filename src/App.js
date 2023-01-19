2084067d54d80fcc74ee1f6cd3eff4cb9b5592bc
RonaldoMonki <ronalldocorrea@gmail.com>
2023-01-18 23:02:19 -0500

    Relajado 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './componets/pages/Layout';
-import Home from './componets/pages/Home';
-import Registro from './componets/pages/Registro';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./componets/pages/Layout";
+import Home from "./componets/pages/Home";
+import Registro from "./componets/pages/Registro";
+import DetailProduct from "./componets/pages/DetailProduct";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />} >
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='registro' element={<Registro />} />
+        <Route path="registro" element={<Registro />} />
+        <Route path="detalleProducto" element={<DetailProduct />} />
       </Route>
     </Routes>
   );
