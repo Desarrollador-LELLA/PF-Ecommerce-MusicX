0ed05c973c603e222f76bb2d4a9779a9cbdb5235
ImOrion <98365044+ImOrion@users.noreply.github.com>
2023-01-19 18:13:14 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./componets/pages/Layout";
-import Home from "./componets/pages/Home";
-import Registro from "./componets/pages/Registro";
-import DetailProduct from "./componets/pages/DetailProduct";
-import Generos from "./componets/pages/Generos";
-import ProductoLista from "./componets/pages/ProductoLista";
-import ProductoCreate from "./componets/pages/ProductoCreate";
-import PerfilUS from "./componets/pages/PerfilU";
-import PerfilAd from "./componets/pages/PerfilAd";
-import InicioSesion from "./componets/pages/InicioSesion";
-import AboutUs from "./componets/pages/AboutUs";
+import { Route, Routes } from 'react-router-dom';
+import Layout from './componets/pages/Layout';
+import Home from './componets/pages/Home';
+import Registro from './componets/pages/Registro';
+import Generos from './componets/pages/Generos';
+import ProductoLista from './componets/pages/ProductoLista';
+import ProductoCreate from './componets/pages/ProductoCreate';
+import PerfilUS from './componets/pages/PerfilU';
+import PerfilAd from './componets/pages/PerfilAd'
+import InicioSesion from './componets/pages/InicioSesion';
+import AboutUs from './componets/pages/AboutUs';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path='/' element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path="registro" element={<Registro />} />
-        <Route path="detalleProducto" element={<DetailProduct />} />
-        <Route path="registro" element={<Registro />} />
-        <Route path="generos" element={<Generos />} />
-        <Route path="producto_lista" element={<ProductoLista />} />
-        <Route path="producto_create" element={<ProductoCreate />} />
-        <Route path="PerfilUsuario" element={<PerfilUS />} />
-        <Route path="PerfilAdmin" element={<PerfilAd />} />
-        <Route path="iniciarsesion" element={<InicioSesion />} />
-        <Route path="AboutUs" element={<AboutUs />} />
+        <Route path='registro' element={<Registro />} />
+        <Route path='generos' element={<Generos />} />
+        <Route path='producto_lista' element={<ProductoLista />} />
+        <Route path='producto_create' element={<ProductoCreate />} />
+        <Route path='PerfilUsuario' element={<PerfilUS />} />
+        <Route path='PerfilAdmin' element={<PerfilAd />} />
+        <Route path="iniciarsesion" element={<InicioSesion/>}/>
+        <Route path= 'AboutUs' element ={<AboutUs/>}/>
       </Route>
     </Routes>
+    
   );
 }
 
