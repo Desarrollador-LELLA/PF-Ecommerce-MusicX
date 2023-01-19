243fc0dd07e010a5d436905f7c8530fbeae3d0da
ImOrion <98365044+ImOrion@users.noreply.github.com>
2023-01-19 09:45:05 -0500

    Reparacion action

--- src/redux/actions/productoAction.js
+++ src/redux/actions/productoAction.js
@@ -2,12 +2,16 @@ import { allAuth, allDb, auth, db } from "../../firebaseInicial/firebase";
 
 const getProductos = async () => {
 
-lista_Productos.forEach((doc) => {
-  console.log(doc.id, " => ", doc.data());
-});
 
-//   Query the first page of docs
-  const first =  await allDb.query(
+  //omitir
+  // const lista_Productos = await allDb.getDocs(allDb.collection(db, "producto"));
+  // lista_Productos.forEach((doc) => {
+  //   console.log(doc.id, " => ", doc.data());
+  // });
+
+
+  //   Query the first page of docs
+  const first = await allDb.query(
     allDb.collection(db, "productos"),
     //orderBy("population"),
     allDb.limit(25)
@@ -15,27 +19,26 @@ lista_Productos.forEach((doc) => {
   const documentSnapshots = await allDb.getDocs(first);
   const todo = await documentSnapshots.docs;
 
+
   // Get the last visible document
-  const lastVisible = await documentSnapshots.docs[documentSnapshots.docs.length - 1];
+  const lastVisible = await documentSnapshots.docs[
+    documentSnapshots.docs.length - 1
+  ];
   // console.log("last", lastVisible);
 
   
   // Construct a new query starting at this document,
   // get the next 25 cities.
-  const next =  await allDb.query(
+  const next = await allDb.query(
     allDb.collection(db, "productos"),
     // orderBy("population"),
     // startAfter(lastVisible),
     allDb.limit(25)
   );
 
-
   return todo;
-
-
 };
 
 export { getProductos };
-
 
 //pa ver si yaxd
