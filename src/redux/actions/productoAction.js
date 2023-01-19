24b8f0853e824bdc97dbcb3d2b04a692f5e1333f
ImOrion <98365044+ImOrion@users.noreply.github.com>
2023-01-19 18:07:58 -0500

    Home

--- src/redux/actions/productoAction.js
+++ src/redux/actions/productoAction.js
@@ -1,9 +1,8 @@
 import { allAuth, allDb, auth, db } from "../../firebaseInicial/firebase";
 
 const getProductos = async () => {
-
-//   Query the first page of docs
-  const first =  await allDb.query(
+  //   Query the first page of docs
+  const first = await allDb.query(
     allDb.collection(db, "productos"),
     //orderBy("population"),
     allDb.limit(25)
@@ -12,26 +11,21 @@ const getProductos = async () => {
   const todo = await documentSnapshots.docs;
 
   // Get the last visible document
-  const lastVisible = await documentSnapshots.docs[documentSnapshots.docs.length - 1];
+  const lastVisible = await documentSnapshots.docs[
+    documentSnapshots.docs.length - 1
+  ];
   // console.log("last", lastVisible);
 
-  
   // Construct a new query starting at this document,
   // get the next 25 cities.
-  const next =  await allDb.query(
+  const next = await allDb.query(
     allDb.collection(db, "productos"),
     // orderBy("population"),
     // startAfter(lastVisible),
     allDb.limit(25)
   );
-
-
   return todo;
-
-
 };
 
 export { getProductos };
-
-
 //pa ver si yaxd
