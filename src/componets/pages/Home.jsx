25ac0d9005f7a37e81927f45c2d473a8eb51126e
Nelsonvargas15 <var.nelson15@gmail.com>
2023-01-20 15:31:54 -0300

    paginate v4

--- src/componets/pages/Home.jsx
+++ src/componets/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getProductos } from '../../redux/actions/productoAction';
+import { getProductos , getTeste } from '../../redux/actions/productoAction';
+import Paginate from '../com/Paginate';
 
 const Home = () => {
     
@@ -11,7 +12,6 @@ const Home = () => {
     const handleclick = async(event)=>{
         event.preventDefault();
         const askdf = await getProductos();
-        console.log(askdf);
         setProductos(askdf)
     }
     
@@ -19,10 +19,9 @@ const Home = () => {
         <div>
             <h2>Aca Se Mostrarian Los Productos en Venta. Esta Ruta es Publica</h2>
             <h1>PROBANDO RPOBANDO</h1>
-            
+            <Paginate></Paginate>
             <button onClick={handleclick} >xd</button>
-           
-            {productos?.map((ele)=>
+           {productos?.map((ele)=>
                 (
                  <span key={ele.data().nombre}>{ele.data().nombre}</span>
                 )
--- src/redux/actions/productoAction.js
+++ src/redux/actions/productoAction.js
@@ -39,6 +39,65 @@ const getProductos = async () => {
   return todo;
 };
 
-export { getProductos };
 
-//pa ver si yaxd
+
+
+const getTeste = async () => {
+// aqui yo creo la consulta 
+  const first = await allDb.query(
+    allDb.collection(db, "productos"),
+    allDb.orderBy("nombre"),
+    allDb.limit(2)
+   
+  );
+  //aqui yo ejecuto la consulta de frist 
+   const documentSnapshots = await allDb.getDocs(first);
+   //extraer los documentos de la consulta para desencriptarlos 
+  const todo = await documentSnapshots.docs;
+ //extrayendo el ultimo documento de la consulta y lo guardo en lastvisible 
+  const lastVisible = await documentSnapshots.docs[
+    documentSnapshots.docs.length - 1
+  ];
+  //extrayendo el primero documento de la consulta y lo guardo en fristvisible
+  const fristVisible = await documentSnapshots.docs[
+    documentSnapshots.docs.length - 2
+  ];
+  //crear la consulta empieza desde cierta parte hacia adelante 
+  const next = await allDb.query(
+    allDb.collection(db, "productos"),
+     allDb.orderBy("nombre"),
+     allDb.startAt(lastVisible),
+    allDb.limit(2)
+  );
+
+  const document = await allDb.getDocs(next);
+  const siguente  = await document.docs;
+
+  const lastVisible2 = await document.docs[
+    document.docs.length - 1
+  ];
+  const fristVisible2 = await document.docs[
+    document.docs.length - 2
+  ];
+// crear la consulta empieza desde cierta parte hacia atras 
+  const previus = await allDb.query(
+    allDb.collection(db, "productos"),
+     allDb.orderBy("nombre"),
+     allDb.startAfter(fristVisible2),
+    allDb.limit(2)
+  );
+  const document3 = await allDb.getDocs(previus);
+  const anterior  = await document3.docs;
+
+   const lastVisible3 = await document3.docs[
+    document3.docs.length - 1
+  ];
+  const fristVisible3 = await document3.docs[
+    document3.docs.length - 2
+  ];
+  
+  return {todo ,anterior, siguente};
+};
+export { getProductos  , getTeste };
+
+
