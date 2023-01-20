29f5556bbaf6d3a95c99a40c234b91f54d4caa94
ImOrion <98365044+ImOrion@users.noreply.github.com>
2023-01-20 14:00:53 -0500

    xd

--- src/componets/pages/Home.jsx
+++ src/componets/pages/Home.jsx
@@ -1,40 +1,7 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { getProductos , getTeste } from '../../redux/actions/productoAction';
-import Paginate from '../com/Paginate';
-
-const Home = () => {
-    
-   
-    const [productos,setProductos]=useState([]);
-
-    const dispatch = useDispatch();
-    const handleclick = async(event)=>{
-        event.preventDefault();
-        const askdf = await getProductos();
-        setProductos(askdf)
-    }
-    
-    return (
-        <div>
-            <h2>Aca Se Mostrarian Los Productos en Venta. Esta Ruta es Publica</h2>
-            <h1>PROBANDO RPOBANDO</h1>
-            <Paginate></Paginate>
-            <button onClick={handleclick} >xd</button>
-           {productos?.map((ele)=>
-                (
-                 <span key={ele.data().nombre}>{ele.data().nombre}</span>
-                )
-            )
-
-            }
-        </div>
-    );
 import ProductCards from "../com/ProductCards";
 import TopBeats from "../com/TopBeats";
-import React, { useEffect, useState } from "react";
-import { getProductos } from "../../redux/actions/productoAction";
 import s from "../../css/Home.module.css";
+import Paginate from '../com/Paginate';
 
 const Home = () => {
   return (
@@ -53,6 +20,7 @@ const Home = () => {
       </p>
       <TopBeats />
       <ProductCards/>
+      <Paginate></Paginate>
     </div>
   );
 };
