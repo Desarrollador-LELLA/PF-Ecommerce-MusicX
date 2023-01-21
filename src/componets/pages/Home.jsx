1dd7552acc1fd1b9d986b3ab6c1d782cc4dfdba3
Nelsonvargas15 <var.nelson15@gmail.com>
2023-01-21 03:25:14 -0300

    pruebas v2

--- src/componets/pages/Home.jsx
+++ src/componets/pages/Home.jsx
@@ -1,35 +1,6 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { getProductos , getTeste } from '../../redux/actions/productoAction';
-import Paginate from '../com/Paginate';
 
-const Home = () => {
-    
-   
-    const [productos,setProductos]=useState([]);
 
-    const dispatch = useDispatch();
-    const handleclick = async(event)=>{
-        event.preventDefault();
-        const askdf = await getProductos();
-        setProductos(askdf)
-    }
-    
-    return (
-        <div>
-            <h2>Aca Se Mostrarian Los Productos en Venta. Esta Ruta es Publica</h2>
-            <h1>PROBANDO RPOBANDO</h1>
-            <Paginate></Paginate>
-            <button onClick={handleclick} >xd</button>
-           {productos?.map((ele)=>
-                (
-                 <span key={ele.data().nombre}>{ele.data().nombre}</span>
-                )
-            )
 
-            }
-        </div>
-    );
 import ProductCards from "../com/ProductCards";
 import TopBeats from "../com/TopBeats";
 import React, { useEffect, useState } from "react";
