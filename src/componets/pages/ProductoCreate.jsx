bdea07911f7c9682bb3804ee798f2d53f3c69722
KennethUC10 <Kenneth.urbina.x@gmail.com>
2023-01-25 12:51:32 -0500

    2023/01/25

--- src/componets/pages/ProductoCreate.jsx
+++ src/componets/pages/ProductoCreate.jsx
@@ -5,6 +5,13 @@ import { Link, useNavigate } from 'react-router-dom';
 import style from '../../css/productoCreate.module.css';
 import { db } from '../../firebaseInicial/firebase';
 import { collection, addDoc } from "firebase/firestore"; 
+import { storage } from '../../firebaseInicial/firebase';
+
+//      Subir imagenes
+//  import { storage } from "./firebase";
+import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
+import { v4 } from "uuid";
+
 
 
 const ProductoCreate = () => {
@@ -16,12 +23,24 @@ const ProductoCreate = () => {
         precio: 0,
         key: "",
         tiempo: 0,
-        imagen: ""
+        //imagen: "",
+        imagen: null
     });
 
+    //  const [image, setImage] = useState(null);
+
+    const [url, setURL] = useState(null);
     const [errores, setErrores] = useState({});
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+
+    //  Segundo ejemplo
+    const [imageUpload, setImageUpload] = useState(null);
+    const [imageUrls, setImageUrls] = useState([]);
+
+    
 
     const ValidoProducto = ({ nombre, autor, descripcion, precio, key, tiempo, imagen }) => {
         const e = {};
@@ -103,6 +122,16 @@ const ProductoCreate = () => {
     }
 
 
+    /*
+    const handleImageChange = (e) => {
+        if(e.target.files[0])
+        {
+            setImage(e.target.files[0]);
+        }
+    }
+    */
+
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setProducto({
@@ -118,20 +147,55 @@ const ProductoCreate = () => {
 
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
         try
         {
+            e.preventDefault();
+
             if(errores.valido)
             {
+                /*
+                const imageRef = ref(storage, "image.jpg");
+
+                uploadBytes(imageRef, producto.imagen, {
+                    contentType: 'image/jpeg'
+                }).then( () => {
+                    setURL(url);
+                }).catch( (err) => {
+                    console.log("Error generado 1 :", err);
+                });
+
+
                 await addDoc(collection(db, "productos"), { ...producto, deshabilitado: false });
                 navigate("/producto_lista");
+                
+                console.log(producto);
+                console.log(imageRef);
+                console.log(producto.imagen);
+                */
+
+
+                //  const imageRef = ref(storage, `images/${ producto.imagen }`);
+
+                const imageRef = ref(storage, `images/${ imageUpload.name + v4() }`);
+
+                uploadBytes(imageRef, imageUpload).then( () => {
+                    setURL(url);
+                }).catch( (err) => {
+                    console.log("Error generado 1 :", err);
+                });
+
+                await addDoc(collection(db, "productos"), { ...producto, deshabilitado: false });
+                console.log(producto);
+                console.log(imageRef);
+                console.log(producto.imagen);
             }
         }
         catch(err)
         {
-            console.log("Error generado :", err);
+            console.log("Error generado 2 :", err);
         }
     }
+
 
     return (
         <div>
@@ -171,9 +235,16 @@ const ProductoCreate = () => {
                         </div>
                         <div className='form-group input-group input-group-text my-3 d-flex justify-content-between'>
                             <Form.Label>Imagen producto :</Form.Label>
+                            {/*
                             <Form.Control name='imagen' type='text' className={ `${ style.textbox } float-right` } placeholder='Ingrese url producto' onChange={ handleInputChange } isInvalid={!!errores.imagen} />
+                            <input name='imagen' type='file' onChange={ (e) => setImageUpload(e.target.values[0]) } />
+                            */}
+
+
+                            <input name='imagen' type='file' onChange={ handleInputChange } />
                             <Form.Control.Feedback type={'invalid'}>{ errores.imagen }</Form.Control.Feedback>
                         </div>
+
                         <div className='form-group input-group input-group-text my-3'>
                             <Button className={ `${ style.button } text-center btn btn-primary` } type='submit' variant='primary' >Registrar</Button>
                         </div>
--- src/firebaseInicial/firebase.js
+++ src/firebaseInicial/firebase.js
@@ -2,15 +2,14 @@ import fireConfig from './config';
 import { initializeApp } from 'firebase/app';
 import * as allAuth from 'firebase/auth';
 import * as allDb from 'firebase/firestore';
-//import { getStorage } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 
 
 const app = initializeApp(fireConfig);
-//const storage = getStorage(app);
+const storage = getStorage(app);
 const auth = allAuth.getAuth(app);
 const db = allDb.getFirestore(app);
 
 export default app;
-export { db, allDb, auth, allAuth, 
-    //storage
+export { db, allDb, auth, allAuth, storage
  };
