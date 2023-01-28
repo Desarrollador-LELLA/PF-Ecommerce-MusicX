7ee3fbc54060a4ab2eb2d6a502d8eb914692a6bf
Luis Llancamil <luis.llancamil.a@gmail.com>
2023-01-28 07:43:22 -0300

    Actualizacion
    ARREGLAO

---
+++ src/utils/estructuraDatos.js
@@ -0,0 +1,62 @@
+const productos = {
+    autor: 'nombre autor',
+    descripcion: 'una descripcion',
+    habilitado: true,
+    imagen: 'link de la imagen',
+    key: 'un key',
+    nombre: 'nombre del producto',
+    tiempo: 180,
+    archivo_prueba: 'link del archivo de prueba',
+    genero: [
+        'genero 1',
+        'genero 2'
+    ],
+    licencias: [
+        {
+            tipo_licencia: 'tipo',
+            descripcion: 'descripcion de la licencia bla bla bla...',
+            precio: 200,
+            archivo: 'link del archivo winrar'
+        }
+    ]
+};
+
+const usuarios = {
+    id: 'id del usuario',
+    nombre: '',
+    apellido: '',
+    descripcion: '',
+    correo: '',
+    rol: '',
+    fechaCreacion: { segunds: 0, miliseguns: 0 },
+    compras: [
+        {
+            fecha: '',
+            productos: [
+                {
+                    id: '',
+
+                }
+            ]
+        }
+    ],
+    biblioteca: [
+        {
+            
+        }
+    ]
+}
+
+const generos = {
+    nombre: 'nombre del genro',
+    habilitado: true
+}
+
+licencias: [
+    {
+        tipo_licencia: 'tipo',
+        descripcion: 'descripcion de la licencia bla bla bla...',
+        precio: 200,
+        archivo: 'link del archivo winrar'
+    }
+];
