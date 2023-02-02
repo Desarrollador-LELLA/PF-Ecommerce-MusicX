6d4ba4751241558e9222dff090d46a3ab47cd462
ImOrion <98365044+ImOrion@users.noreply.github.com>
2023-02-02 18:47:22 -0500

    añskdghauksdhgadiljgbalsdjigasdbgioasdg don culon se puso a jugar lol y no me quiso ayudar

--- src/utils/keysActions.js
+++ src/utils/keysActions.js
@@ -2,7 +2,7 @@ import { allDb, db } from "../firebaseInicial/firebase";
 
 const getKeys = async () => {
     const first = await allDb.query(
-      allDb.collection(db, "Key"),allDb.orderBy("id")
+      allDb.collection(db, "keys"),allDb.orderBy("id")
     );
     const documentSnapshots = await allDb.getDocs(first);
     const todo = await documentSnapshots.docs;
