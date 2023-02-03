5b6c946095301b37a9be4e182b07e9071eabd4fc
Sergio Blanco <108953777+Serblaor@users.noreply.github.com>
2023-02-03 03:12:15 -0500

    Perfil ajustado
    Ya sirven todos los botones

--- src/componets/pages/Perfil.jsx
+++ src/componets/pages/Perfil.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "../../css/perfil.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faPenToSquare, faEnvelope, faLock, faBook, faUserCheck, faCheck} from '@fortawesome/free-solid-svg-icons';
+import { faUser, faPenToSquare, faEnvelope, faLock, faBook, faUserCheck, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { detalle_usuario_cliente } from "../../redux/actions/usuarioAction.js";
 import { useSelector } from "react-redux";
 import { Form } from "react-bootstrap";
 import { actualizaDocumento, subirArchivoMetodo } from "../../utils/metodosFirebase";
 import { Link } from "react-router-dom";
+import ModalChangePas from "./ModalChangePas";
 
 const Perfil = () => {
     const { usuarioAuth } = useSelector(state => state.auth);
     const [datosu, setDatosu] = useState({});
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
         dU();
@@ -30,64 +34,64 @@ const Perfil = () => {
     };
 
     return (
-        <section className="seccion-perfil-usuario">
-            <div className="perfil-usuario-header">
-                <div className="perfil-usuario-portada">
-                    <div className="perfil-usuario-avatar">
-                        <img src={datosu.imagen} alt="img-avatar" width={'166px'} height='166px' />
-                        <button className="boton-avatar">
-                            <i><FontAwesomeIcon icon={faPenToSquare} /></i>
-                            <Form.Control
-                                style={{ opacity: 0 }}
-                                type="file"
-                                accept="image/png, image/jpg, image/jpeg"
-                                color="transparent"
-                                onChange={onchageavatar}
-                            />
-                        </button>
+        <>
+            <section className="seccion-perfil-usuario">
+                <div className="perfil-usuario-header">
+                    <div className="perfil-usuario-portada">
+                        <div className="perfil-usuario-avatar">
+                            <img src={datosu.imagen} alt="img-avatar" width={'166px'} height='166px' />
+                            <button className="boton-avatar">
+                                <i><FontAwesomeIcon icon={faPenToSquare} /></i>
+                                <Form.Control
+                                    style={{ opacity: 0 }}
+                                    type="file"
+                                    accept="image/png, image/jpg, image/jpeg"
+                                    color="transparent"
+                                    onChange={onchageavatar}
+                                />
+                            </button>
+                        </div>
                     </div>
-                    <button type="button" className="boton-portada">
-                        <i><FontAwesomeIcon icon={faPenToSquare} /></i> Cambiar fondo
-                    </button>
                 </div>
-            </div>
-            <div className="perfil-usuario-body">
-                <div className="perfil-usuario-bio text-light">
-                    <h3 className="titulo">{datosu.nombre + " " + datosu.apellido}</h3>
-                    <p className="texto">{datosu.descripcion}</p>
-                </div>
-                <div className="perfil-usuario-footer text-light">
-                    <ul className="lista-datos">
-                        {datosu.rol === "Admin" && <li><i><FontAwesomeIcon icon={faUser} /></i> UsuarioID : {datosu.id} </li>}
-                        <li><i><FontAwesomeIcon icon={faEnvelope} /></i> Correo: {datosu.correo}</li>
-                        {datosu.rol === "Admin" && <li><i><FontAwesomeIcon icon={faCheck} /></i> Rol: {datosu.rol}</li>}
-                    </ul>
-                    <ul className="lista-datos">
-                        <li><i><FontAwesomeIcon icon={faUserCheck} /></i> Registro: {datosu.fechaCreacion && new Date(datosu.fechaCreacion.seconds * 1000).toString()}</li>
-                    </ul>
+                <div className="perfil-usuario-body">
+                    <div className="perfil-usuario-bio text-light">
+                        <h3 className="titulo">{datosu.nombre + " " + datosu.apellido}</h3>
+                        <p className="texto">{datosu.descripcion}</p>
+                    </div>
+                    <div className="perfil-usuario-footer text-light">
+                        <ul className="lista-datos">
+                            {datosu.rol === "Admin" && <li><i><FontAwesomeIcon icon={faUser} /></i> UsuarioID : {datosu.id} </li>}
+                            <li><i><FontAwesomeIcon icon={faEnvelope} /></i> Correo: {datosu.correo}</li>
+                            {datosu.rol === "Admin" && <li><i><FontAwesomeIcon icon={faCheck} /></i> Rol: {datosu.rol}</li>}
+                        </ul>
+                        <ul className="lista-datos">
+                            <li><i><FontAwesomeIcon icon={faUserCheck} /></i> Registro: {datosu.fechaCreacion && new Date(datosu.fechaCreacion.seconds * 1000).toString()}</li>
+                        </ul>
 
-                    {datosu.rol === "Cliente" && <div className="boton-irABiblioteca">
-                        <Link to="/bibloteca">
-                            <button type="onClick" className="boton-biblioteca">
-                                <i><FontAwesomeIcon icon={faBook} /></i> Biblioteca
+                        {datosu.rol === "Cliente" && <div className="boton-irABiblioteca">
+                            <Link to="/bibloteca">
+                                <button type="onClick" className="boton-biblioteca">
+                                    <i><FontAwesomeIcon icon={faBook} /></i> Biblioteca
+                                </button>
+                            </Link>
+                        </div>}
+                        <div className="boton-editar-info">
+                            <Link to={`/editarUsuario/${usuarioAuth.id}`}>
+                                <button type="onClick" className="boton-editar" >
+                                    <i><FontAwesomeIcon icon={faPenToSquare} /></i> editar
+                                </button>
+                            </Link>
+                        </div>
+                        <div className="boton-cambiar-P">
+                            <button type="submit" className="boton-cambiarP" onClick={handleShow}>
+                                <i><FontAwesomeIcon icon={faLock} /></i> cambiar contraseña
                             </button>
-                        </Link>
-                    </div>}
-                    <div className="boton-editar-info">
-                        <Link to={`/editarUsuario/${usuarioAuth.id}`}>
-                            <button type="onClick" className="boton-editar" >
-                                <i><FontAwesomeIcon icon={faPenToSquare} /></i> editar
-                            </button>
-                        </Link>
-                    </div>
-                    <div className="boton-cambiar-P">
-                        <button type="button" className="boton-cambiarP">
-                            <i><FontAwesomeIcon icon={faLock} /></i> cambiar contraseña
-                        </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <ModalChangePas show={show} handleClose={handleClose}/>
+        </>
     );
 };
 export default Perfil;
--- src/utils/metodosFirebase.js
+++ src/utils/metodosFirebase.js
@@ -1,222 +1,277 @@
 //ejemplos de metodos de firebase
-import { allAuth, allDb, auth, db, stor, allStor } from '../firebaseInicial/firebase';
+import {
+  allAuth,
+  allDb,
+  auth,
+  db,
+  stor,
+  allStor,
+} from "../firebaseInicial/firebase";
 
-const retorno = { mensaje: '', result: {}, confirma: false };
+const retorno = { mensaje: "", result: {}, confirma: false };
 
 export const subirArchivo = async (archivoSeleccionado, idUsuario) => {
-    if (archivoSeleccionado == null) return;
-    let imagen = null;
-    const extension = archivoSeleccionado.type.substring(6, archivoSeleccionado.type.length);
-    const imageRef = allStor.ref(stor, `usuarios/avatars/${idUsuario}/avatar.${extension}`);
-    await allStor.uploadBytes(imageRef, archivoSeleccionado).then(async data => {
-        imagen = await mostrarImgen(`usuarios/avatars/${idUsuario}/avatar.${extension}`);
-    }).catch(err => {
-        console.log(err.message);
+  if (archivoSeleccionado == null) return;
+  let imagen = null;
+  const extension = archivoSeleccionado.type.substring(
+    6,
+    archivoSeleccionado.type.length
+  );
+  const imageRef = allStor.ref(
+    stor,
+    `usuarios/avatars/${idUsuario}/avatar.${extension}`
+  );
+  await allStor
+    .uploadBytes(imageRef, archivoSeleccionado)
+    .then(async (data) => {
+      imagen = await mostrarImgen(
+        `usuarios/avatars/${idUsuario}/avatar.${extension}`
+      );
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
-    return imagen;
+  return imagen;
 };
 
 export const subirArchivoMetodo = async (ruta, archivo, pasoFinal) => {
-    if (archivo == null) return; 
-    const imageRef = allStor.ref(stor, ruta);
-    await allStor.uploadBytes(imageRef, archivo).then(async data => {
-        const imagen = await mostrarImgen(ruta);
-        pasoFinal(imagen);
-    }).catch(err => {
-        console.log(err.message)
+  if (archivo == null) return;
+  const imageRef = allStor.ref(stor, ruta);
+  await allStor
+    .uploadBytes(imageRef, archivo)
+    .then(async (data) => {
+      const imagen = await mostrarImgen(ruta);
+      pasoFinal(imagen);
     })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
-
-
 export const mostrarImgen = async (pathArchivo) => {
-    const imagenRef = allStor.ref(stor, pathArchivo);
-    let imagen = null;
+  const imagenRef = allStor.ref(stor, pathArchivo);
+  let imagen = null;
 
-    await allStor.getDownloadURL(imagenRef).then((url) => {
-        imagen = url;
-        //setImageList((prev) => [...prev, url] )
-    }).catch(err => {
-        console.log('ERRORE', err);
+  await allStor
+    .getDownloadURL(imagenRef)
+    .then((url) => {
+      imagen = url;
+      //setImageList((prev) => [...prev, url] )
+    })
+    .catch((err) => {
+      console.log("ERRORE", err);
     });
-    return imagen;
+  return imagen;
 };
 
 /**
  * Este metodo consulta y trae 1 documento en espesifico el cual necesita una coleccion y un id del documento a consultar
- * 
+ *
  * Ejemplo: const r = unDocumento('usuarios', 'iddelcoumento')
- * 
+ *
  * retorna { mensaje: 'string mensaje', result: {}, confirma: false o true }
- * 
+ *
  * @by Don Toulon
  */
 export const unDocumento = async (nombreCollecion, id) => {
-    try {
-        const docRef = allDb.doc(db, nombreCollecion, id);
-        const docSnap = await allDb.getDoc(docRef);
-        if (docSnap.exists()) {
-            retorno.result = docSnap.data();
-            retorno.result.id = docSnap.id;
-            retorno.confirma = true;
-            retorno.mensaje = 'Consulta Exitosa';
-        } else {
-            retorno.result = docSnap;
-            retorno.mensaje = 'La Consulta fue exitosa pero esta no encontro lo que buscabas';
-        }
-    } catch (error) {
-        retorno.mensaje = error.message;
-    } finally {
-        return retorno;
+  try {
+    const docRef = allDb.doc(db, nombreCollecion, id);
+    const docSnap = await allDb.getDoc(docRef);
+    if (docSnap.exists()) {
+      retorno.result = docSnap.data();
+      retorno.result.id = docSnap.id;
+      retorno.confirma = true;
+      retorno.mensaje = "Consulta Exitosa";
+    } else {
+      retorno.result = docSnap;
+      retorno.mensaje =
+        "La Consulta fue exitosa pero esta no encontro lo que buscabas";
     }
+  } catch (error) {
+    retorno.mensaje = error.message;
+  } finally {
+    return retorno;
+  }
 };
-export const unDocumentoCallback = async (nombreCollecion, id, funcionCallback) => {
-    try {
-        const docRef = allDb.doc(db, nombreCollecion, id);
-        const docSnap = await allDb.getDoc(docRef);
-        if (docSnap.exists()) {
-            retorno.result = docSnap.data();
-            retorno.result.id = docSnap.id;
-            retorno.confirma = true;
-            retorno.mensaje = 'Consulta Exitosa';
-        } else {
-            retorno.result = docSnap;
-            retorno.mensaje = 'La Consulta fue exitosa pero esta no encontro lo que buscabas';
-        }
-    } catch (error) {
-        retorno.mensaje = error.message;
-    } finally {
-        funcionCallback(retorno)
+export const unDocumentoCallback = async (
+  nombreCollecion,
+  id,
+  funcionCallback
+) => {
+  try {
+    const docRef = allDb.doc(db, nombreCollecion, id);
+    const docSnap = await allDb.getDoc(docRef);
+    if (docSnap.exists()) {
+      retorno.result = docSnap.data();
+      retorno.result.id = docSnap.id;
+      retorno.confirma = true;
+      retorno.mensaje = "Consulta Exitosa";
+    } else {
+      retorno.result = docSnap;
+      retorno.mensaje =
+        "La Consulta fue exitosa pero esta no encontro lo que buscabas";
     }
+  } catch (error) {
+    retorno.mensaje = error.message;
+  } finally {
+    funcionCallback(retorno);
+  }
 };
-
 
 /**
  * Este metodo consulta y trae varios documento en espesifico el cual necesita una coleccion y una clausula where de los attibutos que necesita comparar.
- * 
+ *
  * e.g "<", "<=", "==", "<", "<=", "!="
- * 
+ *
  * Ejemplo: const r = whereDocumentos('usuarios', {'nombre', '!=', 'pene'})
- * 
+ *
  * retorna { mensaje: 'string mensaje', result: [], confirma: false o true }
- * 
+ *
  * @by Don Toulon
  */
-export const whereDocumentos = async (nombreCollecion, { fieldPath, opStr, value }) => {
-    try {
-        const arrays = [];
-        const q = allDb.query(allDb.collection(db, nombreCollecion), allDb.where(fieldPath, opStr, value));
-        const querySnapshot = await allDb.getDocs(q);
-        querySnapshot.forEach((doc) => {
-            const item = doc.data();
-            item.id = doc.id;
-            arrays.push(item);
-        });
-        retorno.result = arrays;
-        retorno.confirma = true;
-        retorno.mensaje = 'Consulta Exitosa';
-    } catch (error) {
-        retorno.result = [];
-        retorno.mensaje = error.message;
-    } finally {
-        return retorno;
-    }
+export const whereDocumentos = async (
+  nombreCollecion,
+  { fieldPath, opStr, value }
+) => {
+  try {
+    const arrays = [];
+    const q = allDb.query(
+      allDb.collection(db, nombreCollecion),
+      allDb.where(fieldPath, opStr, value)
+    );
+    const querySnapshot = await allDb.getDocs(q);
+    querySnapshot.forEach((doc) => {
+      const item = doc.data();
+      item.id = doc.id;
+      arrays.push(item);
+    });
+    retorno.result = arrays;
+    retorno.confirma = true;
+    retorno.mensaje = "Consulta Exitosa";
+  } catch (error) {
+    retorno.result = [];
+    retorno.mensaje = error.message;
+  } finally {
+    return retorno;
+  }
 };
 
 /**
  * Este metodo crea un documento y retorna el id
- * 
+ *
  * Ejemplo: const r = crearDocumento('usuarios', { data: objeto a crear })
- * 
+ *
  * retorna { mensaje: 'string mensaje', result: {}, confirma: false o true }
- * 
+ *
  * @by Don Toulon
  */
 export const crearDocumento = async (nombreCollecion, { data }) => {
-    try {
-        const docRef = await allDb.addDoc(allDb.collection(db, nombreCollecion), data);
-        retorno.result.id = docRef.id;
-        retorno.confirma = true;
-        retorno.mensaje = 'Consulta Exitosa';
-    } catch (error) {
-        retorno.mensaje = error.message;
-    } finally {
-        return retorno;
-    }
+  try {
+    const docRef = await allDb.addDoc(
+      allDb.collection(db, nombreCollecion),
+      data
+    );
+    retorno.result.id = docRef.id;
+    retorno.confirma = true;
+    retorno.mensaje = "Consulta Exitosa";
+  } catch (error) {
+    retorno.mensaje = error.message;
+  } finally {
+    return retorno;
+  }
 };
 
 /**
  * Este metodo actualiza un documento
- * 
+ *
  * Ejemplo: const r = crearDocumento('usuarios', 'id', { data: objeto a actualizar })
- * 
+ *
  * retorna { mensaje: 'string mensaje', result: {}, confirma: false o true }
- * 
+ *
  * @by Don Toulon
  */
 export const actualizaDocumento = async (nombreCollecion, id, { data }) => {
-    try {
-        const docRef = allDb.doc(db, nombreCollecion, id);
-        const r = await allDb.updateDoc(docRef, data);
+  try {
+    const docRef = allDb.doc(db, nombreCollecion, id);
+    const r = await allDb.updateDoc(docRef, data);
 
-        retorno.result.id = 'docRef.id';
-        retorno.confirma = true;
-        retorno.mensaje = 'Consulta Exitosa';
-    } catch (error) {
-        retorno.mensaje = error.message;
-    } finally {
-        return retorno;
-    }
+    retorno.result.id = "docRef.id";
+    retorno.confirma = true;
+    retorno.mensaje = "Consulta Exitosa";
+  } catch (error) {
+    retorno.mensaje = error.message;
+  } finally {
+    return retorno;
+  }
 };
 
 /**
  * Este metodo consulta y trae varios documento en espesifico el cual necesita una coleccion y una clausula where de los attibutos que necesita comparar.
- * 
+ *
  * e.g "<", "<=", "==", "<", "<=", "!="
- * 
+ *
  * Ejemplo: const r = whereDocumentos('usuarios', {'nombre', '!=', 'pene'})
- * 
+ *
  * retorna { mensaje: 'string mensaje', result: [], confirma: false o true }
- * 
+ *
  * @by Don Toulon
  */
-export const todosDocumentos = async (nombreCollecion, orderBy, whereFiltros, loading) => {
-    try {
-        const arrays = [];
-        let q = null;
-        if (whereFiltros) {
-            q = allDb.query(
-                allDb.collection(db, nombreCollecion),
-                allDb.orderBy(orderBy),
-                allDb.where(whereFiltros)
-            );
-        } else {
-            q = allDb.query(
-                allDb.collection(db, nombreCollecion),
-                allDb.orderBy(orderBy),
-            );
-                
-        }
-
-        const querySnapshot = await allDb.getDocs(q);
-        querySnapshot.forEach((doc) => {
-            const item = doc.data();
-            item.id = doc.id;
-            arrays.push(item);
-        });
-
-        retorno.result = arrays;
-        retorno.confirma = true;
-        retorno.mensaje = 'Consulta Exitosa';
-    } catch (error) {
-        retorno.result = [];
-        retorno.mensaje = error.message;
-    } finally {
-        loading()
-        return retorno;
+export const todosDocumentos = async (
+  nombreCollecion,
+  orderBy,
+  whereFiltros,
+  loading
+) => {
+  try {
+    const arrays = [];
+    let q = null;
+    if (whereFiltros) {
+      q = allDb.query(
+        allDb.collection(db, nombreCollecion),
+        allDb.orderBy(orderBy),
+        allDb.where(whereFiltros)
+      );
+    } else {
+      q = allDb.query(
+        allDb.collection(db, nombreCollecion),
+        allDb.orderBy(orderBy)
+      );
     }
+
+    const querySnapshot = await allDb.getDocs(q);
+    querySnapshot.forEach((doc) => {
+      const item = doc.data();
+      item.id = doc.id;
+      arrays.push(item);
+    });
+
+    retorno.result = arrays;
+    retorno.confirma = true;
+    retorno.mensaje = "Consulta Exitosa";
+  } catch (error) {
+    retorno.result = [];
+    retorno.mensaje = error.message;
+  } finally {
+    loading();
+    return retorno;
+  }
 };
 
+export const PasswordChange = async (newPassword, resultado) => {
+  const user = auth.currentUser;
+  await allAuth.updatePassword(user, newPassword)
+    .then(() => {
+        retorno.result = [];
+        retorno.confirma = true;
+        retorno.mensaje = "Cambio de contraseña exitoso";
+        resultado(retorno)
+    })
+    .catch((error) => {
+        retorno.result = [];
+        retorno.mensaje = error.message;
+        resultado(retorno)
+    });
+};
 
 // EL PAGINADO ME LA MAMA
 // export const paginasPaginado = async (coleccion, orderBy, whereFiltros) => {
@@ -356,7 +411,6 @@ export const todosDocumentos = async (nombreCollecion, orderBy, whereFiltros, lo
 //         lista
 //     };
 // };
-
 
 // export const obtenerPaginas = async (coleccion, orderBy, whereFiltros) => {
 //     let totalDatos = null;
