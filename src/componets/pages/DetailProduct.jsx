441fe04e32e72e9ac39cb2de8381ef5ffc4276c2
RonaldoMonki <ronalldocorrea@gmail.com>
2023-02-08 16:36:31 -0500

    loagind

--- src/componets/pages/DetailProduct.jsx
+++ src/componets/pages/DetailProduct.jsx
@@ -10,13 +10,12 @@ import { useState, useEffect } from "react";
 import { getProducto, addProducto } from "../../redux/actions/carritoAction";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import icMeGusta from '../images/ic_megusta.svg';
-import icNoMeGusta from '../images/ic_nomegusta.svg';
+import icMeGusta from "../images/ic_megusta.svg";
+import icNoMeGusta from "../images/ic_nomegusta.svg";
 import { actualizaDocumento } from "../../utils/metodosFirebase";
 import { arrayRemove, arrayUnion } from "firebase/firestore";
 
 export default function DetailProduct() {
-
   const dispatch = useDispatch();
   const navegar = useNavigate();
   const { id } = useParams();
@@ -25,10 +24,12 @@ export default function DetailProduct() {
   const { productoUnoDetalle } = useSelector((state) => state.carrito);
   const [loadding, setLoadding] = useState(false);
   const [licenNoCompra, setLicenNoCompra] = useState();
-  const [loadingGusta, setLoadingGusta] = useState(false)
+  const [loadingGusta, setLoadingGusta] = useState(false);
 
   useEffect(() => {
-    dispatch(getProducto(id, setLoadding, handlerLicenciasNoCompradas));
+    dispatch(getProducto(id)).then(() => {
+      setLoadding(true);
+    });
   }, []);
 
   function handleAddToCart(producto, e) {
@@ -39,20 +40,19 @@ export default function DetailProduct() {
     dispatch(addProducto(productoAgregar));
     // alert("PRODUCTO AGREGADO AL CARRITO");
   }
-  const handlerLicenciasNoCompradas = (e) => {
-    setLicenNoCompra(e);
+  const handlerLicenciasNoCompradas = () => {
     let auxBiblio = usuarioAuth.biblioteca;
-    if (!auxBiblio) return;
+    console.log("licenciasNoCompradas", licenNoCompra);
+    if (!auxBiblio.length) return;
+    console.log("comparar", licenNoCompra, auxBiblio);
+    let auxArray = [];
     for (let i = 0; i < auxBiblio.length; i++) {
-      if (auxBiblio[i].nombre == productoUnoDetalle.nombre) {
-        //1
-        let aux = licenNoCompra.filter((licen) => {
-          if (licen.TipoLicencia !== auxBiblio[i].licencias.TipoLicencia) {
-            return licen;
-          }
-          return false;
-        });
-        setLicenNoCompra(aux);
+      if (auxBiblio[i].nombre === productoUnoDetalle.nombre) {
+        setLicenNoCompra(
+          licenNoCompra.filter(
+            (licen) => licen.TipoLicencia !== auxBiblio[i].TipoLicencia
+          )
+        );
       }
     }
   };
@@ -80,35 +80,49 @@ export default function DetailProduct() {
   }
 
   const megusta = async () => {
-    setLoadingGusta(true)
+    setLoadingGusta(true);
     if (productoUnoDetalle?.like?.megusta?.includes(usuarioAuth.id)) {
-      const retorno = await actualizaDocumento('productos', id, { data: { 'like.megusta': arrayRemove(usuarioAuth.id) } });
+      const retorno = await actualizaDocumento("productos", id, {
+        data: { "like.megusta": arrayRemove(usuarioAuth.id) },
+      });
       if (retorno.confirma) {
         dispatch(getProducto(id, setLoadding, handlerLicenciasNoCompradas));
       }
     } else {
-      const retorno = await actualizaDocumento('productos', id, { data: { 'like.megusta': arrayUnion(usuarioAuth.id), 'like.nomegusta': arrayRemove(usuarioAuth.id) } });
+      const retorno = await actualizaDocumento("productos", id, {
+        data: {
+          "like.megusta": arrayUnion(usuarioAuth.id),
+          "like.nomegusta": arrayRemove(usuarioAuth.id),
+        },
+      });
       if (retorno.confirma) {
         dispatch(getProducto(id, setLoadding, handlerLicenciasNoCompradas));
       }
     }
-    setLoadingGusta(false)
+    setLoadingGusta(false);
   };
 
   const nomegusta = async () => {
-    setLoadingGusta(true)
+    setLoadingGusta(true);
     if (productoUnoDetalle?.like?.nomegusta?.includes(usuarioAuth.id)) {
-      const retorno = await actualizaDocumento('productos', id, { data: { 'like.nomegusta': arrayRemove(usuarioAuth.id) } });
+      const retorno = await actualizaDocumento("productos", id, {
+        data: { "like.nomegusta": arrayRemove(usuarioAuth.id) },
+      });
       if (retorno.confirma) {
         dispatch(getProducto(id, setLoadding, handlerLicenciasNoCompradas));
       }
     } else {
-      const retorno = await actualizaDocumento('productos', id, { data: { 'like.nomegusta': arrayUnion(usuarioAuth.id), 'like.megusta': arrayRemove(usuarioAuth.id) } });
+      const retorno = await actualizaDocumento("productos", id, {
+        data: {
+          "like.nomegusta": arrayUnion(usuarioAuth.id),
+          "like.megusta": arrayRemove(usuarioAuth.id),
+        },
+      });
       if (retorno.confirma) {
         dispatch(getProducto(id, setLoadding, handlerLicenciasNoCompradas));
       }
     }
-    setLoadingGusta(false)
+    setLoadingGusta(false);
   };
 
   return (
@@ -117,7 +131,9 @@ export default function DetailProduct() {
         <div>
           <div>
             <Container ntainer>
-              <Button className="my-3" onClick={() => navegar(-1)}>Volver</Button>
+              <Button className="my-3" onClick={() => navegar(-1)}>
+                Volver
+              </Button>
               <Row>
                 <Col>
                   <div className={css.box}>
@@ -160,7 +176,7 @@ export default function DetailProduct() {
                   <p className={`text-center ${css.texto}`}>Lista Licencias</p>
                   <div className={`${css.divLicencias} shadow-sm `}>
                     <ListGroup>
-                      {licenNoCompra?.map((obj, indx) => (
+                      {productoUnoDetalle.licencias?.map((obj, indx) => (
                         <div key={indx} onClick={(e) => handlerLicencia(e)}>
                           <Card className={`${css.cardProducto}`}>
                             <Card.Body id={indx} value={obj.precio}>
@@ -185,8 +201,26 @@ export default function DetailProduct() {
                   </div>
                 </Col>
               </Row>
-              <Button className="me-3" onClick={megusta} disabled={loadingGusta}><Image src={icMeGusta} /><span className="mx-3">{productoUnoDetalle?.like?.megusta ? productoUnoDetalle.like.megusta.length : 0}</span></Button>
-              <Button onClick={nomegusta}><span className="mx-3" disabled={loadingGusta}>{productoUnoDetalle?.like?.nomegusta ? productoUnoDetalle.like.nomegusta.length : 0}</span><Image src={icNoMeGusta} /></Button>
+              <Button
+                className="me-3"
+                onClick={megusta}
+                disabled={loadingGusta}
+              >
+                <Image src={icMeGusta} />
+                <span className="mx-3">
+                  {productoUnoDetalle?.like?.megusta
+                    ? productoUnoDetalle.like.megusta.length
+                    : 0}
+                </span>
+              </Button>
+              <Button onClick={nomegusta}>
+                <span className="mx-3" disabled={loadingGusta}>
+                  {productoUnoDetalle?.like?.nomegusta
+                    ? productoUnoDetalle.like.nomegusta.length
+                    : 0}
+                </span>
+                <Image src={icNoMeGusta} />
+              </Button>
             </Container>
             <div className={css.espaciado}></div>
           </div>{" "}
