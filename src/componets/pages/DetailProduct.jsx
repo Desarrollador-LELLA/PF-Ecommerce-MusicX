23e77fce1e43dd1a236630731dbec5a208b30710
RonaldoMonki <ronalldocorrea@gmail.com>
2023-02-09 11:49:21 -0500

    que no se rompa nada

--- src/componets/pages/DetailProduct.jsx
+++ src/componets/pages/DetailProduct.jsx
@@ -138,7 +138,10 @@ export default function DetailProduct() {
                   </div>
                   <div className={css.divgeneros}>
                     <h3 className={` ${css.generos} ${css.tituloProducto}`}>
-                      {productoUnoDetalle.key}
+                      BPM: {productoUnoDetalle.tiempo}
+                    </h3>
+                    <h3 className={` ${css.generos} ${css.tituloProducto}`}>
+                      KEY: {productoUnoDetalle.key}
                     </h3>
                     {productoUnoDetalle.genero?.map((e) => (
                       <h3 className={` ${css.generos} ${css.tituloProducto}`}>
